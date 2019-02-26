083b987f35543a1a507a91c1aac13aaa9d913f0d
manon jacquin <manon.jacquin@gmail.com>
2019-02-26 19:28:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
       supplies = supplies.join(", ")
       holiday = holiday.to_s.split("-").collect {|name| name.capitalize}.join(" ")
     
-      puts 
+      puts "  #{holiday}: #{supplies}"
     end
   end
 end
