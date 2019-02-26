6ffa09347a3c6e91b5fcc0bf9e660f581bd5772a
manon jacquin <manon.jacquin@gmail.com>
2019-02-26 19:25:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,8 +78,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, supplies|
-    supplies.each do |item|
+  
       
 
 end
